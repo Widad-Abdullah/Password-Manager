b7618210dda75dbaa737d76a90212146939be7e7
Widad Abdullah <97903679+Widad-Abdullah@users.noreply.github.com>
2024-09-29 00:15:51 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,26 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint,shuffle,choice
 import pyperclip
+import json
+# -------------------------------- FETCH DATA ----------------------------------- #
+
+def get_data():
+    website=website_entry.get().title()
+    try:
+        with open("PasswordList.json",'r') as l:
+            data_dic=json.load(l)
+    except FileNotFoundError:
+        messagebox.showwarning(title="Empty Data",message="No data file found!\nCreate Records first.")
+    else:
+        if website in data_dic:
+            site_data = data_dic[website]
+            email=site_data["email"]
+            password=site_data["password"]
+            messagebox.showwarning(title="Records Found",message=f"Website: {website}"
+                                                                            f"\nEmail: {email}\nPassword: {password}")
+        else:
+            messagebox.showwarning(title="No Record", message=f"No details for the {website} exists!")
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def password_generator():
@@ -27,24 +47,36 @@ def password_generator():
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save_data():
-    site=website_entry.get()
+    site=website_entry.get().title()
     website_entry.focus()
     email=email_entry.get()
     password=password_entry.get()
+
+    new_data={site:
+              {
+               "email":email,
+                "password":password,
+              }
+    }
 
     if len(site)==0 or len(email)==0 or len(password)==0:
         messagebox.showwarning(title="Empty Fields",message="Don't leave any field empty.")
 
     else:
-        is_ok=messagebox.askyesno(title=site, message=f"The details entered are\nWebsite: {site}\nEmail: {email}\n"
-                            f"Password: {password}\nIs it ok to save?")
-
-        if is_ok:
-            with open("PasswordList.txt",'a') as p:
-                p.write(f"{site} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
-            messagebox.showwarning(title="Sucess",message="Details saved successfully!")
+        try:
+            with open("PasswordList.json",'r') as p:
+                data = json.load(p)
+        except FileNotFoundError:
+            with open("PasswordList.json", 'w') as p:
+                json.dump(new_data, p, indent=4)
+        else:
+            data.update(new_data)
+            with open("PasswordList.json", 'w') as p:
+                json.dump(data, p, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+        messagebox.showwarning(title="Success",message="Details saved successfully!")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -64,8 +96,8 @@ email_label.grid(column=0,row=2)
 password_label=Label(text="Password:")
 password_label.grid(column=0,row=3)
 
-website_entry=Entry(width=50)
-website_entry.grid(column=1,row=1,columnspan=2)
+website_entry=Entry(width=32)
+website_entry.grid(column=1,row=1)
 website_entry.focus()
 email_entry=Entry(width=50)
 email_entry.grid(column=1,row=2,columnspan=2)
@@ -73,6 +105,8 @@ email_entry.insert(0,"widadabdullah@outlook.com")
 password_entry=Entry(width=32)
 password_entry.grid(column=1,row=3)
 
+search_btn=Button(text="Search",width=15,command=get_data)
+search_btn.grid(column=2,row=1)
 add_btn=Button(text="Add",width=43,command=save_data)
 add_btn.grid(column=1, row=4, columnspan=2)
 generate_btn=Button(text="Generate Password",command=password_generator)
